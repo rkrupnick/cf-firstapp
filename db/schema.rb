8c2bd21c5ca63cd7e1e07c74a5402c537a5e9220
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-10-17 23:55:21 -0400

    Added price to products table, restyled index, show and form pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017202221) do
+ActiveRecord::Schema.define(version: 20151018025644) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151017202221) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "color"
+    t.integer  "price"
   end
 
 end
