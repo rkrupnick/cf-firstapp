6b84885f27601e84c25f1a0e2f4862a086dbfce9
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-10-19 00:19:01 -0400

    Added orders controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
+  root 'static_pages#landing_page'
 
   get 'static_pages/about'
 
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  resources :products
+
+  resources :orders, only: [:index, :show, :new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#index'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
