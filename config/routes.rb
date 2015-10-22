7401a44219045d66ed429be163e416c578e74d10
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-10-22 18:07:52 -0400

    Added contact form and thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  post 'static_pages/thank_you'
+
   resources :products
 
   resources :orders, only: [:index, :show, :new, :create]
