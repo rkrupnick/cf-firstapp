b0a9a77aa2eed6af37b98e174f56f1e05f52aadf
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-10-28 20:35:10 -0400

    Added controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe UsersController do
+	before do 
+		@user = User.create(:email => "user@example.com", :password => "password")
+		@user2 = User.create(:email => "user2@example.com", :password => "12345678")
+	end
+	describe "GET #show" do
+		context "User is logged in" do
+			before do
+				sign_in @user
+			end
+
+			it "loads correct user details" do
+				get :show, id: @user.id
+				expect(assigns(:user)).to eq @user
+			end
+
+			it "redirects to home if user tries to access another users show page" do
+				get :show, id: @user2.id
+				expect(response).to redirect_to(root_path)
+			end
+		end
+
+		context "No user is logged in" do
+			it "redirects to login" do
+				get :show, id: @user.id
+				expect(response).to redirect_to(root_path)
+			end
+		end
+	end
+end
