e67bb8a2533d1c0901c536bb31b1b4e0afed4cac
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-11-02 19:56:44 -0500

    Added stripe gem and config

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_1r6ipb83pe8gHisjWsO5e6VQ ',
+    :secret_key => 'sk_test_8koM3AoE1lwOzbD6iJZqFITw '
+  }
+end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
