aee973161d7760a216c0d49ee2406c9e0ddbb00a
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-11-10 16:50:45 -0500

    Added ElevateZoom jQuery plugin to product show pages

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-	$('.alert').delay(1000).fadeOut(600);
-});
-
 $(document).on('ready page:load', function() {
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' } );
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
-			return $(this).attr('data-score');
-		}
-	});
-});
+  // Alert and Notices Fade out
+  $('.alert').delay(1000).fadeOut(600);
+  // Raty
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'});
+  $('.rated').raty({ path: '/assets',
+                   readOnly: true,
+                   score: function() {
+                     return $(this).attr('data-score');
+                   }
+                   });
+  // ElevateZoom
+  $('.img-zoom').elevateZoom({
+  	zoomType: "inner",
+  	cursor: 	"crosshair"
+  });
+}); // End Document Ready Function
