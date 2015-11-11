1b5b8f397648394b0ac967527bc2d95920b6abc8
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-11-10 21:04:48 -0500

    Added Ajax automatic loading for comments.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,13 +2,13 @@ $(document).on('ready page:load', function() {
   // Alert and Notices Fade out
   $('.alert').delay(1000).fadeOut(600);
   // Raty
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'});
-  $('.rated').raty({ path: '/assets',
-                   readOnly: true,
-                   score: function() {
-                     return $(this).attr('data-score');
-                   }
-                   });
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'});
+    $('.rated').raty({ path: '/assets',
+                     readOnly: true,
+                     score: function() {
+                       return $(this).attr('data-score');
+                     }
+                     });
   // ElevateZoom
   $('.img-zoom').elevateZoom({
   	zoomType: "inner",
