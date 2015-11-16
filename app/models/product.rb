a55916f35d2a7768c5199838b1ff345076acb166
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-11-16 17:45:37 -0500

    Added Redis caching for product ranking

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,20 @@ class Product < ActiveRecord::Base
 	def average_rating
 		comments.average(:rating).to_f
 	end
+
+	def compare_rating(average_rating)
+		$redis.zadd("average_ratings", average_rating, self.id)
+	end
+
+	def rank
+		$redis.zrevrank("average_ratings", self.id) + 1
+	end
+
+	def best_product
+		$redis.zscore("average_ratings", self.id).to_i
+	end
+
+	def self.top_3
+		$redis.zrevrange("average_ratings", 0, 2).map{|id| Product.find(id)}
+	end
 end
